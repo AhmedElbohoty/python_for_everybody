6f8d029fa715ba75ea4c4b237df0c7fbad771c58
Ahmed Elbohoty <a.ramzielbohoty@gmail.com>
2020-05-27 11:37:20 +0200

    Exercise ex_05_02

--- first.py
+++ first.py
@@ -24,7 +24,7 @@ else:
 
 
 """ string = "Hello"
-#string = "44"
+# string = "44"
 
 try:
     istr = int(string)
@@ -33,3 +33,53 @@ except:
 
 print(istr) """
 
+""" n = 4
+while n < 9:
+    print(n)
+    n = n + 1 """
+
+""" while True:
+    line = input("> ")
+    if line[0] == "#":
+        print("Don't print")
+        continue
+    if line == "done":
+        break
+    print(line) """
+
+
+""" def largest(numbers):
+    largest = -1
+    for number in numbers:
+        if number > largest:
+            largest = number
+    return largest
+
+
+print(largest([45, 1, 890, 142, 4, 17, 6]))
+print(largest([45, 1, 8, 42, 4, 17, 6])) """
+
+
+def smallest(numbers):
+    smallest = None
+    for number in numbers:
+        if smallest is None:
+            smallest = number
+        elif number < smallest:
+            smallest = number
+    return smallest
+
+
+print(smallest([45, 0, 890, 142, 4, 17, 6]))
+print(smallest([45, 2, -8, 42, 4, 17, 6]))
+
+""" numbers = [14, 5, 99, 77, 13]
+
+found = False
+for i in numbers:
+    if i == 14:
+        found = True
+        break
+    print(i)
+
+print(found) """
